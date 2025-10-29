941706d825d87d8f130f4775b426925e294591ec
maxim <maxim@tecorien.com>
2025-10-29 20:08:33 +0200

    prettier formatting

--- src/templates/backend.js
+++ src/templates/backend.js
@@ -1,6 +1,6 @@
 function getBackendFiles() {
   return {
-    'src/index.ts': `import express from 'express';
+    "src/index.ts": `import express from 'express';
 import { user } from 'shared'
 
 const app = express();
@@ -16,51 +16,60 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(\`Server running on http://localhost:\${PORT}\`);
+  console.log(\`Server running on http://localhost:\${PORT}]\`);
 });`,
 
-    'package.json': JSON.stringify({
-      name: "backend",
-      type: "module",
-      main: "dist/index.js",
-      scripts: {
-        "dev": "tsx watch --tsconfig tsconfig.json src/index.ts",
-        "prebuild": "rimraf dist",
-        "build": "tsc -b && tsc-alias",
-        "start": "node dist/index.js"
+    "package.json": JSON.stringify(
+      {
+        name: "backend",
+        type: "module",
+        main: "dist/index.js",
+        scripts: {
+          dev: "tsx watch --tsconfig tsconfig.json src/index.ts",
+          prebuild: "rimraf dist",
+          build: "tsc -b && tsc-alias",
+          start: "node dist/index.js",
+        },
+        dependencies: {
+          express: "^4.21.1",
+          shared: "workspace:*",
+        },
+        devDependencies: {
+          "@types/express": "^5.0.0",
+          "@types/node": "^22.10.1",
+          "tsc-alias": "^1.8.16",
+          tsx: "^4.19.2",
+          typescript: "^5.6.3",
+        },
       },
-      dependencies: {
-        "express": "^4.21.1",
-        "shared": "workspace:*"
-      },
-      devDependencies: {
-        "@types/express": "^5.0.0",
-        "@types/node": "^22.10.1",
-        "tsc-alias": "^1.8.16",
-        "tsx": "^4.19.2",
-        "typescript": "^5.6.3"
-      }
-    }, null, 2),
 
-    'tsconfig.json': JSON.stringify({
-      compilerOptions: {
-        target: "ES2020",
-        module: "nodenext",
-        outDir: "./dist",
-        rootDir: "./src",
-        strict: true,
-        esModuleInterop: true,
-        skipLibCheck: true,
-        moduleResolution: "nodenext",
-        forceConsistentCasingInFileNames: true,
-        paths: {
-          "@/*": ["./src/*"],
-          "shared": ["../shared/src"]
-        }
+      null,
+      2,
+    ),
+
+    "tsconfig.json": JSON.stringify(
+      {
+        compilerOptions: {
+          target: "ES2020",
+          module: "nodenext",
+          rootDir: "./src",
+          outDir: "./dist",
+          strict: true,
+          esModuleInterop: true,
+          skipLibCheck: true,
+          moduleResolution: "nodenext",
+          forceConsistentCasingInFileNames: true,
+          paths: {
+            "@/*": ["./src/*"],
+            shared: ["../shared/src"],
+          },
+        },
+        include: ["src/**/*"],
+        exclude: ["node_modules", "dist"],
       },
-      include: ["src/**/*"],
-      exclude: ["node_modules", "dist"]
-    }, null, 2)
+      null,
+      2,
+    ),
   };
 }
 
