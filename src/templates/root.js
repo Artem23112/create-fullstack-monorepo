dfb87b4b3d1c734678c9fdf1d22733640d9a8f3a
maxim <maxim@tecorien.com>
2025-10-29 19:05:11 +0200

    Added root file configuration

---
+++ src/templates/root.js
@@ -0,0 +1,33 @@
+function getRootPackageJson(appName) {
+  return {
+    name: appName,
+    scripts: {
+      "dev:frontend": "concurrently -n shared,frontend -c cyan,magenta \"pnpm --filter shared watch:index\" \"pnpm --filter frontend dev\"",
+      "dev:backend": "concurrently -n shared,backend -c cyan,green \"pnpm --filter shared watch:index\" \"pnpm --filter backend dev\"",
+      "build": "pnpm clean && pnpm -r run build",
+      "build:shared": "pnpm --filter shared run build",
+      "build:backend": "pnpm --filter backend run build",
+      "build:frontend": "pnpm --filter frontend run build",
+      "clean": "pnpm -r exec rimraf dist",
+      "start:frontend": "pnpm --filter frontend run preview",
+      "start:backend": "pnpm --filter backend run start"
+    },
+    devDependencies: {
+      "concurrently": "^9.2.1",
+      "prettier": "^3.6.2",
+      "rimraf": "^6.0.1",
+      "typescript": "^5.6.3"
+    }
+  };
+}
+
+function getWorkspaceYaml() {
+  return `packages:
+  - 'packages/*'
+`;
+}
+
+module.exports = {
+  getRootPackageJson,
+  getWorkspaceYaml
+};
